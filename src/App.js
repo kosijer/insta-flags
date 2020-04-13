a5bd3cd7992fba174b99701fe8b0bcf195a0c1c2
kosijer <nebojsa.kosijer@publicissapient.com>
2020-04-14 01:29:29 +0200

    Generates the screen with the flag in the middle and title below. Optimized for 4:5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { flags } from "./data.js";
+import "./App.css";
 
 function App() {
+  const [showFlag, setShowFlag] = useState(false);
+  const [selectedFlag, setSelectedFlag] = useState({});
+  const [search, setSearch] = useState("");
+  console.log("search", flags);
+  const windowHeight = window.innerHeight - 40;
+  function generateFlage(flag) {
+    setSelectedFlag(flag);
+    setShowFlag(true);
+  }
+
+  const styles = {
+    container: {
+      height: windowHeight,
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showFlag ? (
+        <>
+          <input
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            type="text"
+          />
+          {Object.keys(flags).map((flagcode) => {
+            if (
+              flags[flagcode].name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return (
+                <button
+                  className="flagButton"
+                  onClick={() => generateFlage(flags[flagcode])}
+                >
+                  {flags[flagcode].emoji} - {flags[flagcode].name}
+                </button>
+              );
+            }
+            return null;
+          })}
+        </>
+      ) : (
+        <div style={styles.container} className="container">
+          <span onClick={() => setShowFlag(false)}>{selectedFlag.emoji}</span>
+          <p>{selectedFlag.name}</p>
+        </div>
+      )}
     </div>
   );
 }
