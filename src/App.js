e00bd3d6530345c5f68c811b24d668ea9cdcd9fa
Nebojsa Kosijer <kosijer@gmail.com>
2020-04-14 01:49:50 +0200

    More height decreasing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [selectedFlag, setSelectedFlag] = useState({});
   const [search, setSearch] = useState("");
   console.log("search", flags);
-  const windowHeight = window.innerHeight - 80;
+  const windowHeight = window.innerHeight - 110;
   function generateFlage(flag) {
     setSelectedFlag(flag);
     setShowFlag(true);
